01d28c46b3fb7043607a2124569443be0ee50854
Parnassius <Parnassius@users.noreply.github.com>
2025-09-17 14:07:43 +0200

    Tests: simplify database setup

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-import os
-from pathlib import Path
 from typing import TYPE_CHECKING
 
 import pytest
@@ -17,15 +15,7 @@ if TYPE_CHECKING:
 
 @pytest.fixture(scope="session", autouse=True)
 def setup_database(tmp_path_factory: pytest.TempPathFactory) -> None:
-    if env_id := os.environ.get("HATCH_ENV_ACTIVE"):
-        cache = Path(__file__).parent / ".database_cache"
-        if not cache.exists():
-            cache.mkdir()
-            (cache / ".gitignore").write_text("*")
-        database_path = cache / f"{env_id}.sqlite"
-    else:
-        database_path = tmp_path_factory.mktemp("data") / "pokedex.sqlite"
-    pokedex.setup_database(database_path)
+    pokedex.setup_database(tmp_path_factory.getbasetemp() / "pokedex.sqlite")
 
 
 @pytest.fixture
